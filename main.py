2c8d8be005764380d811bf5559b2346f17952971
Shasanka Gautam <shasankagautam@mail.weber.edu>
2025-04-01 09:56:39 -0600

    FINAL: Done

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from animal import Animal
+from dog import Dog
 
 if __name__ == "__main__":
     # TODO: Create an instance of the Animal class
+    bird = Animal("Big Bird", "Ostrich") 
     # TODO: Print the Animal instance
+    print(bird)
     # TODO: Call the method to make a generic animal sound
+    bird.speak()
 
     # TODO: Create an instance of the Dog class
+    fido = Dog("Fido", "Canine", "Hound")
     # TODO: Print the Dog instance
+    print(fido)
     # TODO: Call the method to make the dog-specific sound
+    fido.speak()
 
     # TODO print out all the animals
+    animals = Animal.get_all_animals()
+    for animal in animals:
+        print(animal)
